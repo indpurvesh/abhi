bf59d222d2cb008ad5c68074b0af8e82aba63a3a
Purvesh <ind.purvesh@gmail.com>
2018-07-28 18:01:36 +1200

    added two unit test

---
+++ tests/AppTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests;
+use Tests\TestCase;
+
+class App extends TestCase
+{
+    /**
+     * An App Run Test.
+     *
+     * @return void
+     */
+    public function testAppRun()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+class TestCase extends PHPUnitTestCase 
+{
+
+    public function setup() 
+    {
+        parent::setUp();
+    }
+
+
+}
